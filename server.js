aee80f73767f24470d403151f9eb4f07be2bd64a
Eugene Tan <jmingtan@gmail.com>
2016-06-23 00:30:51 +0800

    collect request times as metric

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const metrics = require('metrics');
 
 const app = express();
+const metricsServer = new metrics.Server(9091);
+
+const requests = new metrics.Timer();
+metricsServer.addMetric('requests', requests);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    const start = Date.now();
+    for (let i=0; i<1000; i++)
+      res.write('Hello World!');
+    res.end();
+    console.log(Date.now() - start);
+    requests.update(Date.now() - start);
 });
 
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000, metrics on :9091/metrics!');
 });
